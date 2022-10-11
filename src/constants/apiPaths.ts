7385ac76329bf0ff5fe0b62f84ae9b114a8800af
Ilya Prikazchikov <ilia_prikazchikov@epam.com>
2022-10-12 00:56:15 +0300

    chore: update urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://bdbcfbae76.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-};
+  product: 'https://fdbzp4x411.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://06kpzmlefk.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+}
 
 export default API_PATHS;
